3ba15848fc1573f04257fa94470f03f4ef667242
fcomarcet1 <fcomarcet1@gmail.com>
2022-02-18 13:14:13 +0100

    Add: Implement Idea Model and relations OneToMany to user,

--- app/Http/Controllers/Idea/IdeaController.php
+++ app/Http/Controllers/Idea/IdeaController.php
@@ -11,8 +11,6 @@ class IdeaController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -21,8 +19,6 @@ class IdeaController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -31,9 +27,6 @@ class IdeaController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\Idea\StoreIdeaRequest  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(StoreIdeaRequest $request)
     {
@@ -42,9 +35,6 @@ class IdeaController extends Controller
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Models\Idea  $idea
-     * @return \Illuminate\Http\Response
      */
     public function show(Idea $idea)
     {
@@ -53,9 +43,6 @@ class IdeaController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Idea  $idea
-     * @return \Illuminate\Http\Response
      */
     public function edit(Idea $idea)
     {
@@ -64,10 +51,6 @@ class IdeaController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\Idea\UpdateIdeaRequest  $request
-     * @param  \App\Models\Idea  $idea
-     * @return \Illuminate\Http\Response
      */
     public function update(UpdateIdeaRequest $request, Idea $idea)
     {
@@ -76,9 +59,6 @@ class IdeaController extends Controller
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Idea  $idea
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Idea $idea)
     {
--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Idea extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    protected $guarded = [];
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+    /**
+     * Get the user that owns the idea.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
