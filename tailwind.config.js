b9726dac2f4b5a9281369629e0471f03fd650d45
fcomarcet1 <fcomarcet1@gmail.com>
2022-02-18 10:20:59 +0100

    Config: Config styles in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-    content: [
+    purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -9,11 +10,50 @@ module.exports = {
 
     theme: {
         extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+
+                black: colors.black,
+                white: colors.white,
+                gray: colors.trueGray,
+                'gray-background': '#f7f8fc',
+                'blue': '#328af1',
+                'blue-hover': '#2879bd',
+                'yellow' : '#ffc73c',
+                'red' : '#ec454f',
+                'green' : '#1aab8b',
+                'purple' : '#8b60ed',
+            },
+            spacing: {
+                44: '11rem',
+                70: '17.5rem',
+                175: '43.75rem',
+            },
+            maxWidth: {
+                custom: '62.5rem',
+            },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
+            },
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xxs: ['0.625rem', { lineHeight: '1rem' }],
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };
