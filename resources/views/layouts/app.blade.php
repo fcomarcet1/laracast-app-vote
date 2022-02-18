f9a4d863507948995cd2cbba30c95caa52d81db2
fcomarcet1 <fcomarcet1@gmail.com>
2022-02-18 10:17:33 +0100

    Add logo in app.blade.php and img folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,94 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laracast Voting App') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans text-gray-900 text-sm">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laracast Voting App') }}</title>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<!-- Original body -->
+{{--<body class="font-sans text-gray-900 text-sm">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+        <!-- Page Heading -->
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>--}}
+<!-- End Original body -->
+<body class="font-sans bg-gray-background text-gray-900 text-sm">
+    <header class="flex items-center justify-between px-8 py-4">
+        <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
+        <div class="flex items-center">
+            @if (Route::has('login'))
+                <div class="px-6 py-4">
+                    @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">
+                                {{ __('Log out') }}
+                            </a>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
                 </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @endif
+            <a href="#">
+                <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+            </a>
         </div>
-    </body>
+    </header>
+    {{--<main class="container mx-auto max-w-custom flex">
+        <div class="w-70 mr-5">
+            Add idea form goes here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officiis
+            architecto quis similique velit suscipit eius voluptate officia, repudiandae perspiciatis maiores placeat.
+            Praesentium rem officia, asperiores minima totam, commodi mollitia vitae aliquid nisi deleniti ipsa autem quos
+            alias corporis?
+        </div>
+        <div class="w-175">
+            <nav class="flex items-center justify-between text-xs">
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li>
+                        <a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a>
+                    </li>
+                </ul>
+
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li>
+                        <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="mt-8">
+                {{ $slot }}
+            </div>
+        </div>
+    </main>--}}
+</body>
 </html>
