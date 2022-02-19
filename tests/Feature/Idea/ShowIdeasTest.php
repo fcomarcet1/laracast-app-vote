9eb356b87d7f2dc410335805ef7dfcaa444fa5dc
fcomarcet1 <fcomarcet1@gmail.com>
2022-02-19 10:30:46 +0100

    Add Test: Add Ideas test singleIdeaShowsInDetailIdeaPageTest, ideasPaginationWorksTest, sameIdeaTitleAndDifferentSlugs

--- tests/Feature/Idea/ShowIdeasTest.php
+++ tests/Feature/Idea/ShowIdeasTest.php
@@ -7,6 +7,7 @@ use App\Models\Idea;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+// TODO: Add tests
 class ShowIdeasTest extends TestCase
 {
     use RefreshDatabase;
@@ -36,7 +37,82 @@ class ShowIdeasTest extends TestCase
         $response->assertSee($secondIdea->description);
         $response->assertStatus(200);
 
+    }
+
+    // test for single idea shows in detail idea page
+    /** @test */
+    public function singleIdeaShowsInDetailIdeaPageTest(): void
+    {
+        //create idea
+        $idea = Idea::factory()->create([
+            'title' => 'First idea',
+            'description' => 'First idea description',
+        ]);
+
+        // create response
+        $response = $this->get(route('idea.show', $idea));
+
+        // check response
+        $response->assertSuccessful();
+        $response->assertSee($idea->title);
+        $response->assertSee($idea->description);
+        $response->assertStatus(200);
+    }
+
+    //test for ideas pagination
+    /** @test */
+    public function ideasPaginationWorksTest(): void
+    {
+        // create paginate ideas(11)
+        $ideas = Idea::factory(Idea::PAGINATION_COUNT + 1)->create();
+
+        $firstIdea = Idea::find(1);
+        $firstIdea->title = 'First idea';
+        $firstIdea->description = 'First idea description';
+        $firstIdea->save();
+
+        $elevenIdea = Idea::find(11);
+        $elevenIdea->title = 'Eleven idea';
+        $elevenIdea->description = 'Eleven idea description';
+        $elevenIdea->save();
+
+        // create response for check I can see first 10 ideas and don't see 11 in page 1
+        $response = $this->get('/');
+        $response->assertSee($firstIdea->title);
+        $response->assertDontSee($firstIdea->title);
+
+        // create response for check I can see 11 in page 2 and don't see firstideas.
+        $response = $this->get('/?page=2');
+        $response->assertSee($elevenIdea->title);
+        $response->assertDontSee($firstIdea->title);
 
     }
+
+    /** @test */
+    public function sameIdeaTitleAndDifferentSlugs(): void
+    {
+        // create ideas
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Another Description for my first idea',
+        ]);
+
+        $response = $this->get(route('idea.show', $ideaOne));
+        $response->assertSuccessful();
+        $response->assertStatus(200);
+        $this->assertSame(request()->path(), 'ideas/my-first-idea');
+
+        $response = $this->get(route('idea.show', $ideaTwo));
+        $response->assertSuccessful();
+        $response->assertStatus(200);
+        $this->assertSame(request()->path(), 'ideas/my-first-idea-1');
+    }
+
+
 
 }
