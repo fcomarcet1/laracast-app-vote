66b4eae123b11bc3076d93e07f2172b24d0c0979
fcomarcet1 <fcomarcet1@gmail.com>
2022-02-20 11:32:21 +0100

    Add: Add status field in  Idea::with('user', 'category', 'status') at IdeaController@index

--- app/Http/Controllers/Idea/IdeaController.php
+++ app/Http/Controllers/Idea/IdeaController.php
@@ -18,7 +18,9 @@ class IdeaController extends Controller
             'ideas' => Idea::with('user', 'category')
                 ->simplePaginate(Idea::PAGINATION_COUNT),
         ]);*/
-        $ideas = Idea::with('user', 'category')->simplePaginate(Idea::PAGINATION_COUNT);
+        $ideas = Idea::with('user', 'category', 'status')
+            ->simplePaginate(Idea::PAGINATION_COUNT);
+
         return view('idea.index', compact('ideas'));
     }
 
