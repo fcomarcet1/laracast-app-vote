6e71a1a3c24e6273466a512412d9e8429e0b1b3e
fcomarcet1 <fcomarcet1@gmail.com>
2022-02-25 09:55:05 +0100

    Add: Add vote function in livewire/indexIdea and showIdea for ++ or -- vote counter

--- app/Http/Livewire/IndexIdea.php
+++ app/Http/Livewire/IndexIdea.php
@@ -18,6 +18,26 @@ class IndexIdea extends Component
         $this->hasVoted = $idea->voted_by_user; // voted_by_user is an index used in index action for addSelect
     }
 
+    public function vote()
+    {
+        // check if user is not logged in and redirect to login page
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+
+        // TODO: refactor this
+        if ($this->hasVoted) {
+            $this->idea->removeVote(auth()->user());
+            $this->votesCount--;
+            $this->hasVoted = false;
+        } else {
+            $this->idea->addVote(auth()->user());
+            $this->votesCount++;
+            $this->hasVoted = true;
+        }
+
+    }
+
     public function render()
     {
         return view('livewire.index-idea');
--- resources/views/livewire/show-idea.blade.php
+++ resources/views/livewire/show-idea.blade.php
@@ -51,11 +51,15 @@
                             <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
                         </div>
                         @if ($hasVoted)
-                            <button class="w-20 bg-blue text-white border border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5">
+                            <button
+                                wire:click.prevent="vote"
+                                class="w-20 bg-blue text-white border border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5">
                                 Voted
                             </button>
                         @else
-                            <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">
+                            <button
+                                wire:click.prevent="vote"
+                                class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">
                                 Vote
                             </button>
                         @endif
